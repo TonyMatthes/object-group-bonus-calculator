82483e77fbe7961dd05d25a55061c4aa3122aa93
Adam Fieldson <atfieldson@gmail.com>
2018-08-28 16:07:13 -0500

    core plus first stretch goal

--- client.js
+++ client.js
@@ -16,6 +16,93 @@ const mayella =  new Employee( 'Mayella', '89068', '35000', 2 );
 const employees = [ atticus, jem, scout, robert, mayella ]; // this is an array of objects
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+$(document).ready(readyNow)
+
+let modifiedEmployeeInfo = []
+
+function employeeInfo(){
+  for ( employee of employees){
+    console.log(employeeInfoIndividual(employee));
+    modifiedEmployeeInfo.push(employeeInfoIndividual(employee));
+    console.log(modifiedEmployeeInfo);
+  }
+  return true //because luke
+}
+
+function employeeInfoIndividual(employee){
+ //need other functions 
+ let employeeSalaryInfo = {
+   name: employee.name,
+   bonusPercentage: bonusPercentage( employee ),
+   totalCompensation: totalSalary( employee),
+   totalBonus: totalBonus( employee )
+ }
+return employeeSalaryInfo;
+}
+
+
+function bonusPercentage( employee ){
+  //add bonus percentage from rating
+  let bonusPercent = 0;
+  if (employee.reviewRating === 3) {
+    bonusPercent += .04;
+  }
+  else if (employee.reviewRating === 4) {
+    bonusPercent += .06;
+  }
+  else if (employee.reviewRating === 5) {
+    bonusPercent += .1;
+  }
+  //console.log( ' Review based bonus =', bonusPercent);
+  
+  //check for additional bonus from employee number/tenure
+  if (employee.employeeNumber.length === 4){
+    bonusPercent += .05;
+    //console.log( 'They received tenure bonus of 5%!  New total bonus % is', bonusPercent );
+  }
+  //check if salary above 65,000
+   //adjust bonus down 1%
+  if ( Number(employee.salary) > 65000 ){
+    bonusPercent -= .01;
+    // console.log( 'New employee bonus:', bonusPercent, '.  Took away 1% for making too much money.' );
+  }
+  //check to make sure bonus is 0% < total bonus < 13%
+  if ( bonusPercent < 0 ){
+    bonusPercent = 0;
+    // console.log('Less than 0 : ', bonusPercent);
+  }
+  else if ( bonusPercent > .13 ){
+    bonusPercent = .13;
+    // console.log('greater than 13 : ', bonusPercent);
+  }
+  return bonusPercent;
+}
+
+//totalBonus integer rounded to nearest integer
+
+function totalBonus ( employee ) {
+  let bonusInDollars = bonusPercentage(employee) * employee.annualSalary;
+  // console.log( Math.round(bonusInDollars));
+  return Math.round(bonusInDollars);
+}
+
+//totalCompensation adding salary to totalBonus
+
+function totalSalary( employee ){
+  return totalBonus( employee ) + Number(employee.annualSalary);
+}
+
+//display employees information on DOM
+
+function readyNow(){
+  console.log( 'in readyNow' );
+  employeeInfo();
+  console.log(modifiedEmployeeInfo);
+  for(employee of modifiedEmployeeInfo){
+    $( '#employeeOutput' ).append( '<li>'+employee.name + ', ' + employee.bonusPercentage + ', ' + employee.totalCompensation + ', ' + employee.totalBonus + '</li>')
+  }
+}
+
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -24,4 +111,4 @@ const employees = [ atticus, jem, scout, robert, mayella ]; // this is an array 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+// console.log( employees );
